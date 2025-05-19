4d73e2e78d66f58e805c346cdd679fde40f25061
Husen00 <hmhusen1998@gmail.com>
2025-05-19 15:09:06 +0200

    Implement a project for The LCD1602 is a character type liquid crystal display that can display 32 (16*2) characters simultaneously through I2C bus.

---
+++ Projects in Python/I2C-LCD1602/I2C-LCD1602.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import LCD1602  # Modul für die Schnittstelle mit LCD1602 importieren
+import time     # Modul für Timing-Funktionen importieren
+
+def setup():
+    # LCD mit I2C-Adresse 0x27 initialisieren und Hintergrundbeleuchtung aktivieren
+    LCD1602.init(0x27, 1)
+    # Die Nachricht 'Greetings!' in der oberen linken Ecke anzeigen (Zeile 0, Spalte 0)
+    LCD1602.write(0, 0, 'Greetings!')
+    # Die Nachricht 'From SunFounder' in der zweiten Zeile anzeigen (Zeile 1, Spalte 1)
+    LCD1602.write(1, 1, 'From SunFounder')
+    time.sleep(2)  # Nachrichten für 2 Sekunden anzeigen
+
+try:
+    setup()  # Setup-Funktion ausführen, um das LCD zu initialisieren und Nachrichten anzuzeigen
+
+except KeyboardInterrupt:
+    # LCD-Display löschen, wenn eine Tastaturunterbrechung (z. B. Ctrl+C) auftritt
+    LCD1602.clear()
+    pass  # Ohne weitere Aktion fortfahren
